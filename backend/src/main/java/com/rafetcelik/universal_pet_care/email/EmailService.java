7b65b49b63e82546bb5cd5c39b98e62300d8f83b
Rafet Celik <blaxdrafet@gmail.com>
2026-02-15 01:05:30 +0300

    Refactor: Move email secrets to properties and remove hardcoded credentials

--- backend/src/main/java/com/rafetcelik/universal_pet_care/email/EmailService.java
+++ backend/src/main/java/com/rafetcelik/universal_pet_care/email/EmailService.java
@@ -1,49 +1,36 @@
 package com.rafetcelik.universal_pet_care.email;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Properties;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class EmailService {
-	
-	private JavaMailSender mailSender;
-	
-	@PostConstruct
-	private void init() {
-		mailSender = createMailSender();
-	}
-	
-	public void sendEmail(String to, String subject, String senderName, String mailContent) throws UnsupportedEncodingException, MessagingException {
-		MimeMessage message = mailSender.createMimeMessage();
-		var messageHelper = new MimeMessageHelper(message);
-		messageHelper = new MimeMessageHelper(message, "UTF-8");
-		messageHelper.setFrom(EmailProperties.DEFAULT_USERNAME, senderName);
-		messageHelper.setTo(to);
-		messageHelper.setSubject(subject);
-		messageHelper.setText(mailContent, true);
-		mailSender.send(message);
-	}
-	
-	private JavaMailSender createMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost(EmailProperties.DEFAULT_HOST);
-		mailSender.setPort(EmailProperties.DEFAULT_PORT);
-		mailSender.setUsername(EmailProperties.DEFAULT_USERNAME);
-		mailSender.setPassword(EmailProperties.DEFAULT_PASSWORD);
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.smtp.auth", String.valueOf(EmailProperties.DEFAULT_AUTH));
-		props.put("mail.smtp.starttls.enable", String.valueOf(EmailProperties.DEFAULT_STARTTLS_ENABLE));
-		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-	    props.put("mail.debug", "true");
-		return mailSender;
-	}
+    
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    public void sendEmail(String to, String subject, String senderName, String mailContent) 
+            throws UnsupportedEncodingException, MessagingException {
+        
+        MimeMessage message = mailSender.createMimeMessage();
+        
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+        
+        messageHelper.setFrom(senderEmail, senderName);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        
+        mailSender.send(message);
+    }
 }
